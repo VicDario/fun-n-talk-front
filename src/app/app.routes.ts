7f8fef48254b2913395f9e4b8ccf59550503c9f2
VicDario <26585356+VicDario@users.noreply.github.com>
2025-02-27 15:20:44 -0300

    feat: add routing with join screen page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./join-screen/join-screen.component').then((c) => c.JoinScreenComponent) },
+];
