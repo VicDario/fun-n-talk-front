258969f71a14b724c77cdcad188bc26e981c970b
VicDario <26585356+VicDario@users.noreply.github.com>
2025-02-27 15:20:04 -0300

    feat: add join screen page

---
+++ src/app/join-screen/join-screen.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ChatService } from '../services/chat/chat.service';
+
+@Component({
+  selector: 'app-join-screen',
+  imports: [FormsModule],
+  templateUrl: './join-screen.component.html',
+  styleUrl: './join-screen.component.css',
+})
+export class JoinScreenComponent {
+  private readonly _router = inject(Router);
+  private readonly _chatService = inject(ChatService);
+
+  public joinRoom(event: SubmitEvent): void {
+    const target = event.target as HTMLFormElement;
+    this._chatService.userName = target['userName'].value;
+    this._chatService.groupName = target['groupName'].value;
+    this._chatService
+      .startConnection()
+      .then(() => this._router.navigate(['/chat-room']));
+  }
+}
