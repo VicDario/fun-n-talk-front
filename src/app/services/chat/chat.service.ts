4a10888a38ccd5227416bb63a3d495378f5518f8
VicDario <26585356+VicDario@users.noreply.github.com>
2025-02-28 20:32:41 -0300

    feat: chat service manage data and consumes signalr service

--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -1,96 +1,66 @@
-import { Injectable } from '@angular/core';
-import {
-  HubConnection,
-  HubConnectionBuilder,
-  LogLevel,
-} from '@microsoft/signalr';
-import { BehaviorSubject } from 'rxjs';
-import type { Message } from '@interfaces/message.interface';
+import { inject, Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '@env/environment';
+import { SignalRService } from '@services/signal-r/signal-r.service';
+import type { Message } from '@interfaces/message.interface';
+import type { User } from '@interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChatService {
-  private readonly _hubConnection: HubConnection;
+  private readonly _signalRService = inject(SignalRService);
+  private readonly _http = inject(HttpClient);
   private _userName?: string;
-  private _groupName?: string;
-  private _messages = new BehaviorSubject<Message[]>([]);
-
-  constructor() {
-    this._hubConnection = new HubConnectionBuilder()
-      .withUrl(environment.hubUrl)
-      .configureLogging(LogLevel.Information)
-      .withKeepAliveInterval(5000)
-      .withAutomaticReconnect()
-      .build();
-
-    this.addEvents();
-  }
+  private _roomName?: string;
+  private _participants = signal<User[]>([]);
+  private _messages = signal<Message[]>([]);
 
   public set userName(value: string) {
     this._userName = value;
   }
 
-  public get connectionId() {
-    return this._hubConnection.connectionId;
+  public set roomName(value: string) {
+    this._roomName = value;
   }
 
-  public set groupName(value: string) {
-    this._groupName = value;
+  public async initConnection(): Promise<void> {
+    await this._signalRService.startConnection();
+    await this._signalRService.joinRoom(this._roomName!, this._userName!);
+    this.getParticipants();
+    this.onMessageEvent();
   }
 
-  public async startConnection() {
-    try {
-      await this._hubConnection.start();
-      if (!this.isConnected) throw new Error('Connection failed');
-      this.joinRoom();
-    } catch (error) {
-      return console.error(error);
-    }
+  public stopConnection(): void {
+    this._signalRService.stopConnection(this._roomName!);
   }
 
-  public async stopConnection() {
-    try {
-      await this._hubConnection.stop();
-      if (this.isConnected) throw new Error('Disconnection failed');
-    } catch (error) {
-      return console.error(error);
-    }
+  private getParticipants(): void {
+    this._http
+      .get<User[]>(`${environment.apiUrl}/api/communication/room/${this._roomName}/participants`)
+      .subscribe((participants) => this._participants.set(participants));
   }
 
-  public addEvents() {
-    this._hubConnection.on('UserJoined', (username: string) => {
-      console.log(username);
-    });
+  public sendMessage(message: string): void {
+    this._signalRService.sendMessage(this._roomName!, message);
+  }
 
-    this._hubConnection.on('UserLeft', (connectionId: string) => {
-      console.log('User with connectionId ' + connectionId + ' has left.');
-    });
-
-    this._hubConnection.on('ReceiveMessage', (message: Message) => {
-      this._messages.next([...this._messages.getValue(), message]);
+  private onMessageEvent(): void {
+    this._signalRService.messageEventEmitter.subscribe((message) => {
+      console.log(message);
+      this._messages.update((messages) => [...messages, message]);
     });
   }
 
-  public async joinRoom() {
-    await this._hubConnection.invoke(
-      'JoinRoom',
-      this._groupName,
-      this._userName
-    );
+  public get connectionId(): string {
+    return this._signalRService.connectionId ?? '';
   }
 
-  public async sendMessage(message: string) {
-    console.log('Sending message: ' + message);
-    await this._hubConnection.invoke('SendMessage', this._groupName, message);
+  public get messages() {
+    return this._messages;
   }
 
-  public get $messages() {
-    return this._messages.asObservable();
-  }
-
-  public get isConnected() {
-    return this._hubConnection.state === 'Connected';
+  public get participants() {
+    return this._participants;
   }
 }
