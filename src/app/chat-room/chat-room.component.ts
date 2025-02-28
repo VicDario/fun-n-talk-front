a755843380f717555a096beafc06cdc2f19c9788
VicDario <26585356+VicDario@users.noreply.github.com>
2025-02-27 23:04:26 -0300

    feat: add chat room page

---
+++ src/app/chat-room/chat-room.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from '@angular/core';
+import { ChatService } from '@services/chat/chat.service';
+import { FormsModule } from '@angular/forms';
+import { ChatSidebarComponent } from '@app/chat-sidebar/chat-sidebar.component';
+import { VideoGridComponent } from '@app/video-grid/video-grid.component';
+import { Router } from '@angular/router';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-chat-room',
+  imports: [FormsModule, ChatSidebarComponent, VideoGridComponent, NgClass],
+  templateUrl: './chat-room.component.html',
+  styleUrl: './chat-room.component.css',
+})
+export class ChatRoomComponent {
+  private readonly _chatService = inject(ChatService);
+  private readonly _router = inject(Router);
+  public messageToSend = '';
+  public participants: any[] = [];
+  public isVideoEnabled = true;
+  public isMicrophoneEnabled = true;
+
+  public leaveRoom(): void {
+    this._chatService.stopConnection();
+    this._router.navigate(['/']);
+  }
+
+  public toggleAudio(): void {
+    return;
+  }
+
+  public toggleVideo(): void {
+    return;
+  }
+}
