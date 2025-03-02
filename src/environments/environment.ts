2e998f781cc0a89bc722cb11a510668cfc76343d
VicDario <26585356+VicDario@users.noreply.github.com>
2025-03-01 23:01:05 -0300

    feat: update api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://funntalk-1058570323303.us-central1.run.app',
+  apiUrl: 'https://fun-n-talk-back.netlify.app',
   iceServers: [
     {
       urls: 'stun:stun.l.google.com:19302',
