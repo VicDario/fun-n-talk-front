612ae9bbc0e24b536d31640e9819da156d15cc6e
VicDario <26585356+VicDario@users.noreply.github.com>
2025-03-01 21:55:37 -0300

    feat: get remote streams and show

--- src/app/video-grid/video-grid.component.ts
+++ src/app/video-grid/video-grid.component.ts
@@ -1,17 +1,36 @@
 import { NgClass } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, signal } from '@angular/core';
+import { MediaService } from '@services/media/media.service';
+import { WebRtcService } from '@services/web-rtc.service';
 
 @Component({
   selector: 'app-video-grid',
   imports: [NgClass],
   templateUrl: './video-grid.component.html',
-  styleUrl: './video-grid.component.css'
+  styleUrl: './video-grid.component.css',
 })
 export class VideoGridComponent {
-  @Input() participants: any[] = [];
+  private readonly _mediaService = inject(MediaService);
+  private readonly _webRtcService = inject(WebRtcService);
+  public readonly localStream = signal<MediaStream | null>(null);
+  public isVideoEnabled = true;
+  public isAudioEnabled = true;
 
-  get gridClass() {
-    const count = this.participants.length;
+  ngOnInit() {
+    this.startLocalStream();
+  }
+
+  async startLocalStream() {
+    const stream = await this._mediaService.getLocalStream();
+    this.localStream.set(stream);
+  }
+
+  public get remoteStreams() {
+    return this._webRtcService.remoteStreams;
+  }
+
+  public get gridClass() {
+    const count = this.remoteStreams().length;
     if (count <= 1) return 'grid-cols-1';
     if (count <= 4) return 'grid-cols-2';
     if (count <= 9) return 'grid-cols-3';
