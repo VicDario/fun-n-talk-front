9d97def083fa997a4ecb6f1511447a68423566e4
VicDario <26585356+VicDario@users.noreply.github.com>
2025-03-06 14:41:53 -0300

    feat: when user is not allow navigate to base path

--- src/app/guards/chat-room.guard.ts
+++ src/app/guards/chat-room.guard.ts
@@ -1,8 +1,12 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { SignalRService } from '@services/signal-r/signal-r.service';
 
 export const chatRoomGuard: CanActivateFn = () => {
   const service = inject(SignalRService);
-  return service.isConnected;
+  const router = inject(Router);
+  const { isConnected } = service;
+  if (!isConnected) return router.navigate(['/']);
+
+  return isConnected;
 };
