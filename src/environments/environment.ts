4e9555ff4f6ddce3b1f457cabe345a2d784532c6
VicDario <26585356+VicDario@users.noreply.github.com>
2025-03-06 14:14:56 -0300

    feat: set ice servers for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,27 @@ export const environment = {
   apiUrl: 'https://funntalk-1058570323303.us-central1.run.app',
   iceServers: [
     {
-      urls: 'stun:stun.l.google.com:19302',
+      urls: 'stun:stun.relay.metered.ca:80',
+    },
+    {
+      urls: 'turn:sa.relay.metered.ca:80',
+      username: 'acb4f434d300cbec6c741c96',
+      credential: 'zwFadx6AO+f+iO7x',
+    },
+    {
+      urls: 'turn:sa.relay.metered.ca:80?transport=tcp',
+      username: 'acb4f434d300cbec6c741c96',
+      credential: 'zwFadx6AO+f+iO7x',
+    },
+    {
+      urls: 'turn:sa.relay.metered.ca:443',
+      username: 'acb4f434d300cbec6c741c96',
+      credential: 'zwFadx6AO+f+iO7x',
+    },
+    {
+      urls: 'turns:sa.relay.metered.ca:443?transport=tcp',
+      username: 'acb4f434d300cbec6c741c96',
+      credential: 'zwFadx6AO+f+iO7x',
     },
   ],
 };
