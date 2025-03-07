ba86963e110805171e12750b027ac9ef5d47f655
VicDario <26585356+VicDario@users.noreply.github.com>
2025-03-07 14:21:14 -0300

    feat: improve form to validate inputs

--- src/app/join-screen/join-screen.component.ts
+++ src/app/join-screen/join-screen.component.ts
@@ -1,24 +1,71 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ChatService } from '../services/chat/chat.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-join-screen',
-  imports: [FormsModule],
+  imports: [NgClass, ReactiveFormsModule],
   templateUrl: './join-screen.component.html',
   styleUrl: './join-screen.component.css',
 })
 export class JoinScreenComponent {
   private readonly _router = inject(Router);
   private readonly _chatService = inject(ChatService);
+  public joinForm: FormGroup;
 
-  public joinRoom(event: SubmitEvent): void {
-    const target = event.target as HTMLFormElement;
-    this._chatService.userName = target['userName'].value;
-    this._chatService.roomName = target['roomName'].value;
+  constructor() {
+    this.joinForm = new FormGroup({
+      userName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      roomName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    });
+  }
+
+  public onSubmit(): void {
+    if (this.joinForm.invalid) {
+      this.joinForm.markAllAsTouched();
+      return;
+    }
+    this.joinRoom(this.joinForm.value);
+    this.joinForm.reset();
+  }
+
+  public joinRoom({
+    userName,
+    roomName,
+  }: {
+    userName: string;
+    roomName: string;
+  }): void {
+    this._chatService.userName = userName;
+    this._chatService.roomName = roomName;
     this._chatService
       .initConnection()
       .then(() => this._router.navigate(['/chat-room']));
+  }
+
+  public isValidField(field: string): boolean | null {
+    return (
+      this.joinForm.controls[field].errors &&
+      this.joinForm.controls[field].touched
+    );
+  }
+
+  public getFieldError(field: string): string | null {
+    if (!this.joinForm.controls[field]) return null;
+
+    const errors = this.joinForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'This field is required!';
+
+        case 'minlength':
+          return `Needs at least ${errors['minlength'].requiredLength} characters.`;
+      }
+    }
+    return null;
   }
 }
